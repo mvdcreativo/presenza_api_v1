c97cd859f949733f50584f6548c3da7fc6018360
unknown <mvdcreativo@gmail.com>
2020-08-06 10:12:32 -0300

    seeds

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,18 +11,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
-        // $this->call(CountrySeeder::class);
-        // $this->call(ProvinceSeeder::class);
-        // $this->call(CitySeeder::class);
-        // $this->call(MunicipalitySeeder::class);
-        // $this->call(NeighborhoodSeeder::class);
-        // $this->call(ExpenseSeeder::class);
-        // $this->call(FeatureSeeder::class);
-        // $this->call(StatusSeeder::class);
-        // $this->call(TaxSeeder::class);
+        $this->call(UserSeeder::class);
+        $this->call(CountrySeeder::class);
+        $this->call(ProvinceSeeder::class);
+        $this->call(CitySeeder::class);
+        $this->call(MunicipalitySeeder::class);
+        $this->call(NeighborhoodSeeder::class);
+        $this->call(ExpenseSeeder::class);
+        $this->call(FeatureSeeder::class);
+        $this->call(StatusSeeder::class);
+        $this->call(TaxSeeder::class);
         $this->call(CurrencySeeder::class);
-        // $this->call(TransactionTypeSeeder::class);
-        // $this->call(PropertyTypeSeeder::class);
+        $this->call(TransactionTypeSeeder::class);
+        $this->call(PropertyTypeSeeder::class);
     }
 }
