7e345210d06644a32d2c7732ccf659e9a50a9651
Emir <mvdcreativo@gmail.com>
2021-11-23 13:16:49 -0300

    mails

--- app/Mail/MessageContact.php
+++ app/Mail/MessageContact.php
@@ -43,6 +43,6 @@ class MessageContact extends Mailable
         // ->bcc($address, $name)
         // ->replyTo($address, $name)
         ->subject($subject)
-        ->with([ 'test_message' => $this->data['message'] ]);
+        ->with($this->msg);
     }
 }
